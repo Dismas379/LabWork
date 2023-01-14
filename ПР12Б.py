71c8958492a2914d665e6928d13cdc303b45607f
Dismas379 <122639851+Dismas379@users.noreply.github.com>
2023-01-14 05:14:19 +0300

    Add files via upload

---
+++ ПР12Б.py
@@ -0,0 +1,14 @@
+def hgf():
+    x = int(input())
+    if x == 0:
+        return x
+    else:
+        max1=0
+        max2=0
+        if x!=0:
+            if x>max1:
+                max2=max1
+                max1=x
+            elif a>max2:
+                max2=x
+print(hgf())
