d3e591202403d64135284662651ebd7719b2be82
Dismas379 <122639851+Dismas379@users.noreply.github.com>
2023-01-14 05:01:28 +0300

    Delete 9laba.py

--- 9laba.py
+++
@@ -1,3 +0,0 @@
-with open('Астафьев_Михаил_Романович_ЗИТ_122_vvod.txt','r') as firstfile, open('Астафьев_Михаил_Романович_ЗИТ_122_vivod.txt','a') as secondfile:
-    for line in firstfile:
-        secondfile.write(line)
